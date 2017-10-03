38f9d7c76d2ae448928b445262d7af54badd6b5f
Oscar Perez <31116879+osedpesa@users.noreply.github.com>
2017-10-03 11:24:18 -0500

    Update PruebaPila.cpp

--- PruebaPila.cpp
+++ PruebaPila.cpp
@@ -2,15 +2,19 @@
 #include "Lista.h"
 #include "Pila.h"
 using namespace std;
+
 main (){
 	Stack p; 
 	DATA data;
-	cout<<"Ingresar Valor a La Pila:"<<endl;
+	
+	cout << "Ingresar Valor a La Pila:" << endl;
+	
 	for(int i=0; i<3; i++){
-    	cin>>data;
-	    p.insertDataStack(data);
+    		cin >> data;
+		p.insertDataStack(data);
 	}
-	cout<<endl;
+	
+	cout << endl;
 	p.print();	
 }
 
